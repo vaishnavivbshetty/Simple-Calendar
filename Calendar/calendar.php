047a9d0544010f1913637ad12ee133cbb3c31367
Vaishnavi V B <84962671+vaishnavivbshetty@users.noreply.github.com>
2021-06-16 22:00:04 +0530

    Add files via upload

---
+++ Calendar/calendar.php
@@ -0,0 +1,108 @@
+<?php
+ define("ADAY", (60*60*24));
+ if ((!isset($_POST['month'])) || (!isset($_POST['year']))) {
+ $nowArray = getdate();
+ $month = $nowArray['mon'];
+$year = $nowArray['year'];
+ } else {
+$month = $_POST['month'];
+$year = $_POST['year'];
+}
+ $start = mktime (12, 0, 0, $month, 1, $year);
+ $firstDayArray = getdate($start);
+ ?>
+ <!DOCTYPE html>
+<html>
+ <head>
+ <title><?php echo "Calendar: ".$firstDayArray['month']."
+".$firstDayArray['year']; ?></title>
+ <style type="text/css">
+ table {
+ border: 12x solid black;
+ border-collapse: collapse;
+ }
+th {
+ border: 2px solid black;
+ padding: 6px;
+ font-weight: bold;
+ background: pink;
+ }
+ td {
+ border: 2px solid black;
+ padding: 6px;
+ vertical-align: top;
+ width: 100px;
+ }
+ h1{
+	 
+	 font-family:calibri;
+	 color:blue;
+ }
+ form{
+	
+	 font-family:calibri;
+	 color:pink;
+ }
+ body{
+	 
+	 font-family:calibri;
+	 background-color:pink;
+ }
+ </style>
+ </head>
+ <body>
+ <h1 >Select a Month/Year Combination</h1>
+ <form method="post" action="<?php echo $_SERVER["PHP_SELF"];?>">
+ <select name="month" style="background-color:skyblue;color:black">
+ <?php
+ $months = Array("January", "February", "March", "April", "May",
+ "June", "July", "August", "September", "October", "November", "December");
+for ($x=1; $x <= count($months); $x++) {
+echo"<option value=\"$x\"";
+ if ($x == $month) {
+echo " selected";
+}
+ echo ">".$months[$x-1]."</option>";
+ }
+?>
+ </select>
+ <select name="year" style="background-color:skyblue;color:black">
+ <?php 
+ for ($x=1980; $x<=2010; $x++) {
+echo "<option";
+ if ($x == $year) {
+echo " selected";
+ }
+ echo ">$x</option>";
+ }
+?>
+</select>
+ <button style="background-color:skyblue;color:black" type="submit" name="submit" value="submit">Go!</button>
+ </form>
+ <br>
+ <?php 
+ $days = Array("Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat");
+ echo "<table><tr>\n";
+ foreach ($days as $day) {
+echo "<td>".$day."</td>\n";
+ }
+ for ($count=0; $count < (6*7); $count++) {
+ $dayArray = getdate($start);
+ if (($count % 7) == 0) {
+ if ($dayArray['mon'] != $month) {
+ break;
+ } else {
+ echo "</tr><tr>\n";
+ }
+ }
+ if ($count < $firstDayArray['wday'] || $dayArray['mon'] != $month) {
+ echo "<td>&nbsp;</td>\n";
+ } else {
+ echo "<td>".$dayArray['mday']."</td>\n";
+ $start += ADAY;
+ }
+ }
+ echo "</tr></table>";
+ ?>
+ </body>
+ </html>
